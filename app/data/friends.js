b0aafae72187b45ae7f558f9040bf18bf4d03232
Monroe Howard II <monroehowardii@Monroes-MacBook-Air.local>
2018-12-22 19:05:53 -0600

    added modal and json format to friendsjs

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,18 @@
-var friendsArray = [
-    {
-      Name: "",
-      Photo: "",
-      Scores: []
-    }
-  ];
+var userData = {
+  name: $("#name").val(),
+  photo: $("#photo").val(),
+  scores: [
+    $("#q1").val(),
+    $("#q2").val(),
+    $("#q3").val(),
+    $("#q4").val(),
+    $("#q5").val(),
+    $("#q6").val(),
+    $("#q7").val(),
+    $("#q8").val(),
+    $("#q9").val(),
+    $("#q10").val()
+  ]
+};
 
-  module.exports = friendsArray;
+module.exports = userData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,13 +3,18 @@ express = require("express");
 var router = express.Router();
 var data = require("./apiRoutes");
 
-router.get("/friends", function(req,res){
+router.get("/friends", function (req, res) {
     res.status(200).json(data);
 });
 
-router.post("/friends", function(req,res){
+router.post("/friends", function (req, res) {
     data.push(req.body);
-    res.status(200).json({msg: "Your profile was added successfully!"});
+    res.status(200).json({ msg: "Your profile was added successfully!" });
+    $("#match-name").text(data.name);
+    $("#match-img").attr("src", data.photo);
+
+    // Show the modal with the best match
+    $("#results-modal").modal("toggle");
 });
 
 module.exports = router;
